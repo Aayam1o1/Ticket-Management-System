5d1a3a23983ba410071be45bd7fad09faf3132d2
Aayam1o1 <aayamm.calcgen@gmail.com>
2025-08-10 16:45:47 +0545

    feat(readme): Added proper guidelines for the readme file and improved changes in datascript.py

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # Ticket-Management-System
+
+## Setup
+
+The first thing to do is to clone the repository:
+
+```sh
+$ git clone git@github.com:Aayam1o1/Ticket-Management-System.git
+$ cd ticket-management-system
+```
+
+Create a virtual environment using conda, activate it and install dependencies using poetry:
+
+```sh
+$ conda create --name <env> python=3.11
+$ conda activate <env>
+```
+
+Then install the dependencies:
+
+```sh
+(env)$ poetry install
+```
+
+Note the `(env)` in front of the prompt. This indicates that this terminal
+session operates in a virtual environment set up by `conda`.
+
+Once `poetry` has finished downloading the dependencies:
+
+```sh
+
+(env)$ cd project
+(env)$ python manage.py migrate
+(env)$ python datascript.py
+
+(env)$ python manage.py runserver
+```
+
+Create a .env file and add all the necessary environment variable present in the .env.template file.
+And navigate to `http://127.0.0.1:8000/admin/`.
+
+## Features
+
+- Roles and Permissions
+- Menu Creation and Assign
+- Ticket Creation and Assign
+
+Note: The admin panel can be accessed by two users: 
+username: superuser
+password: superpass1234
+
+username: admin
+password: pass1234
+
+The permissions are set as:
+admin: Can do everything mentioned in the permission model.
+supervisor: Create, edit and view (ticket and menu) and Assign the menu and tickets.
+agent: Can Create and view ticket
+normal: Can view ticket.
--- datascript.py
+++ datascript.py
@@ -10,7 +10,6 @@ from account.models import Role, Permission, User
 from menu.models import Menu
 
 def run():
-    # Permissions (all 11)
     permissions_data = [
         ("can_create_ticket", "Allows user to can create ticket"),
         ("can_edit_ticket", "Allows user to can edit ticket"),
@@ -21,9 +20,7 @@ def run():
         ("can_manage_menu", "Allows user to can manage menu"),
         ("can_create_roles", "Allow user to create new roles"),
         ("can_manage_permissions", "Allow user to manage permissions"),
-        # The following two permissions you mentioned in the list but not in your previous code - 
-        # if you want to add any more, you can include here. For now all 9 are here.
-        # If you want to add e.g. can_manage_users, add here.
+        
     ]
 
     for ptype, desc in permissions_data:
@@ -67,61 +64,65 @@ def run():
 
     # Users
     users_data = [
-        {
-            "username": "admin",
-            "email": "aayammaharjan5@gmail.com",
-            "phone_number": "+9779840033590",
-            "role": role_objs["Admin"],
-            "is_staff": False,
-            "is_active": True,
-            "password": "pass1234",
-        },
-        {
-            "username": "agent",
-            "email": "aayamm.calcgen@gmail.com",
-            "phone_number": "+9779840033590",
-            "role": role_objs["Agent"],
-            "is_staff": False,
-            "is_active": True,
-            "password": "pass1234",
-        },
-        {
-            "username": "normal",
-            "email": "nopermission@gamil.com",
-            "phone_number": "+9779840033590",
-            "role": role_objs["normal"],
-            "is_staff": False,
-            "is_active": True,
-            "password": "pass1234",
-        },
-        {
-            "username": "supervisor",
-            "email": "supervisor@gamil.com",
-            "phone_number": "+9779840033590",
-            "role": role_objs["Supervisor"],
-            "is_staff": False,
-            "is_active": True,
-            "password": "pass1234",
-        },
-    ]
+    {
+        "username": "admin",
+        "email": "aayammaharjan5@gmail.com",
+        "phone_number": "+9779840000000",
+        "role": role_objs["Admin"],
+        "is_staff": True,        
+        "is_superuser": True,     
+        "is_active": True,
+        "password": "pass1234",
+    },
+    {
+        "username": "agent",
+        "email": "aayamaharjan5@gmail.com",
+        "phone_number": "+9779840000000",
+        "role": role_objs["Agent"],
+        "is_staff": False,
+        "is_superuser": False,
+        "is_active": True,
+        "password": "pass1234",
+    },
+    {
+        "username": "normal",
+        "email": "nopermission@gamil.com",
+        "phone_number": "+9779840000000",
+        "role": role_objs["normal"],
+        "is_staff": False,
+        "is_superuser": False,
+        "is_active": True,
+        "password": "pass1234",
+    },
+    {
+        "username": "supervisor",
+        "email": "supervisor@gamil.com",
+        "phone_number": "+9779840000000",
+        "role": role_objs["Supervisor"],
+        "is_staff": False,
+        "is_superuser": False,
+        "is_active": True,
+        "password": "pass1234",
+    },
+]
 
     for udata in users_data:
         user, created = User.objects.get_or_create(username=udata["username"], defaults={
             "email": udata["email"],
             "phone_number": udata["phone_number"],
             "role": udata["role"],
-            "is_staff": udata["is_staff"],
+            "is_staff": udata.get("is_staff", False),
+            "is_superuser": udata.get("is_superuser", False),
             "is_active": udata["is_active"],
         })
         if created:
             user.set_password(udata["password"])
             user.save()
         else:
-            # Update existing user info
             updated = False
-            for field in ["email", "phone_number", "role", "is_staff", "is_active"]:
-                if getattr(user, field) != udata[field]:
-                    setattr(user, field, udata[field])
+            for field in ["email", "phone_number", "role", "is_staff", "is_superuser", "is_active"]:
+                if getattr(user, field) != udata.get(field, getattr(user, field)):
+                    setattr(user, field, udata.get(field, getattr(user, field)))
                     updated = True
             if updated:
                 user.save()
